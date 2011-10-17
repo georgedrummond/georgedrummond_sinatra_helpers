a5cd5184d4624358c3d635be2ac50c0bbfaff4fb
George Drummond <george@accountsapp.com>
2011-10-18 00:20:51 +0100

    add some documentation

--- georgedrummond_sinatra_helpers.gemspec
+++ georgedrummond_sinatra_helpers.gemspec
@@ -8,15 +8,15 @@ Gem::Specification.new do |s|
   s.authors     = ["George Drummond"]
   s.email       = ["george@accountsapp.com"]
   s.homepage    = "https://github.com/georgedrummond/georgedrummond_sinatra_helpers"
-  s.summary     = %q{Common helper methods}
-  s.description = %q{Some common helper methods I use in my apps}
+  s.description = %q{This is a collection of common helper methods that I use within my Sinatra apps. They have a rails like feel and they mainly fill in for helpers you would find in Rails. They do not behave exactly like their rails counterparts so please be careful to read the RDOC.}
+  s.summary     = s.description
 
+  s.extra_rdoc_files = ['README.rdoc', 'LICENSE']
+  s.rdoc_options      = %w[--line-numbers --inline-source --title Sinatra --main README.rdoc]
+  
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  # specify any dependencies here; for example:
-  # s.add_development_dependency "rspec"
-  # s.add_runtime_dependency "rest-client"
 end
--- lib/georgedrummond_sinatra_helpers.rb
+++ lib/georgedrummond_sinatra_helpers.rb
@@ -3,7 +3,22 @@ require "georgedrummond_sinatra_helpers/version"
 module GeorgeDrummond
   module Sinatra
     module Helpers
-    
+      # Creates html tags for stylesheets from the arguements provided. Dont include the <tt>.css</tt> extension as this
+      # will be automatically appended. You can specify one or multiple css files at once. Place your css files in
+      # the <tt>public/css</tt> folder of your app.
+      #
+      # ==== Examples
+      #
+      # With only one CSS file
+      #
+      #    stylesheet_link_tag :app
+      #    # => <link href="/css/app.css" type="text/css" />
+      #
+      # With an array of CSS files we want to show
+      #
+      #    stylesheet_link_tag :app, :header # =>
+      #      <link href="/css/app.css" type="text/css" />
+      #      <link href="/css/header.css" type="text/css" />
       def stylesheet_link_tag(*sources)
         html = []
         sources.each do |stylesheet|
@@ -12,7 +27,23 @@ module GeorgeDrummond
         end
         return html.join("\n")
       end
-    
+      
+      # Creates html tags for javascript includes from the arguments provided. Dont include the <tt>.js</tt> extension as this
+      # will be automatically appended. You can specify one or multiple javascript files at once. Place your javascript files in
+      # the <tt>public/js</tt> folder of your app.
+      #
+      # ==== Examples
+      #
+      # With only one javascript file
+      #
+      #    javascript_include_tag :app
+      #    # => <script type="text/javascript" src="/js/app.js"></script>
+      #
+      # With an array of javascript files we want to show
+      #
+      #    javascript_include_tag :jquery, :app # =>
+      #      <script type="text/javascript" src="/js/jquery.js"></script>
+      #      <script type="text/javascript" src="/js/app.js"></script>
       def javascript_include_tag(*sources)
         html = []
         sources.each do |javascript|
@@ -21,12 +52,26 @@ module GeorgeDrummond
         end
         return html.join("\n")
       end
-    
+      
+      # Creates a html image tag given a file name. File should be located in <tt>public/images</tt>. Will not work with external
+      # files for now however I may add this at a later date if it is needed.
+      #
+      # ==== Examples
+      #
+      #    image_tag "home.png"
+      #    # => <img src="/images/home.png" />
       def image_tag(image)
         path = "/images/#{image}"
         return "<img src=\"#{url(path)}\" />\n"
       end
     
+      # Creates a html link tag given a page <tt>title</tt> and resource <tt>path</tt>. Works similar to the Rails link_to method
+      # however does not accept any other options.
+      #
+      # ==== Examples
+      #
+      #    link_to "Home", "/"
+      #    # => <a href="/">Home</a>
       def link_to(title, path)
         return "<a href=\"#{url(path)}\">#{title}</a>"
       end
